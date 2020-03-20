57fb31d07de3833a76f09914597a49000fbc7a2f
Muhammad Nizwa <muhammad.nizwa@gmail.com>
2020-03-20 23:53:22 +0700

    fixing glide

--- app/src/main/java/com/example/simplenews/util/BindingAdapters.kt
+++ app/src/main/java/com/example/simplenews/util/BindingAdapters.kt
@@ -5,6 +5,8 @@ import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.simplenews.R
 
 /**
  * Binding adapter used to hide the spinner once data is available
@@ -23,7 +25,10 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         Glide.with(imgView.context)
             .load(imgUri)
-            .fitCenter()
+            .apply(RequestOptions()
+                .placeholder(R.drawable.loading_animation)
+                .error(R.drawable.ic_broken_image)
+                .override(1000, 1000))
             .into(imgView)
     }
 }
