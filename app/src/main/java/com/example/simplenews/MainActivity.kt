f4061427624258c3d0b51d68b1e7b4aafe56a894
Muhammad Nizwa <muhammad.nizwa@gmail.com>
2020-03-20 16:40:47 +0700

    create appbar

--- app/src/main/java/com/example/simplenews/MainActivity.kt
+++ app/src/main/java/com/example/simplenews/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.simplenews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_item, menu)
+        return true
     }
 }
